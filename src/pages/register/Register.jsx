8427f0b7d152cb7255fbf14b7358b71c04da9555
fatihozkul35 <fatihozkul3541@gmail.com>
2022-03-30 18:21:26 +0200

    I'm stuck on the registration page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, Input } from "../login/LoginStyles";
-
+import { signIn } from "../../auth/firebase";
 const Register = () => {
+  const [formRegister, setFormRegister] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleFormRegister = () => {};
+
   const handleRegister = (e) => {
     e.preventDefault();
-
-    console.log("handleRegister calıstı");
+    signIn();
   };
   return (
     <div>
       <h1>Register page</h1>
       <Form>
-        <Input type="text" placeholder="Enter your firsname" />
-        <Input type="text" placeholder="Enter your lastname" />
-        <Input type="email" placeholder="Enter your email" />
-        <Input type="password" placeholder="Enter your password" />
+        <Input
+          type="text"
+          name="firstName"
+          placeholder="Enter your firsname"
+          value={formRegister.firstName}
+          onChange={handleFormRegister}
+        />
+        <Input
+          type="text"
+          name="lastName"
+          placeholder="Enter your lastname"
+          value={formRegister.lastName}
+          onChange={handleFormRegister}
+        />
+        <Input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          value={formRegister.email}
+          onChange={handleFormRegister}
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          value={formRegister.password}
+          onChange={handleFormRegister}
+        />
         <Button type="submit" onClick={handleRegister}>
           SIGNIN
         </Button>
