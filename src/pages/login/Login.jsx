a7808fb98af29450fa0a8d6145a333c669f18d40
fatihozkul35 <fatihozkul3541@gmail.com>
2022-06-09 17:56:21 +0200

    login and register created

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./LoginStyles";
+import mealSvg from "../../assets/meal.svg";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
+  const user = {
+    username: "user",
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(user));
+    navigate("/");
+  };
 
-export default Login
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={mealSvg} />
+        <Header>{"<Fatih/>"}Recipe</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput type="text" placeholder="username" required />
+          <StyledInput type="password" placeholder="password" required />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
+
+export default Login;
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import underConst from "../../assets/const.png";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div>
+      <img style={{ width: "90%", margin: "4rem 0" }} src={underConst} alt="" />
+    </div>
+  );
+};
 
-export default Register
+export default Register;
