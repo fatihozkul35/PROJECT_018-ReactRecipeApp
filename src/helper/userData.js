14afb6c2d4d9c5c458818a3fed1ff0c3f6421a28
fatihozkul35 <fatihozkul3541@gmail.com>
2022-06-09 14:50:51 +0200

    I deleted the project and I'm starting over.

--- src/helper/userData.js
+++
@@ -1,8 +0,0 @@
-export const userLogInDatas = [
-  { id: 1, userName: "fatih", password: "fatih" },
-  { id: 2, userName: "mustafa", password: "mustafa" },
-  { id: 3, userName: "osman", password: "osman" },
-  { id: 4, userName: "halil", password: "halil" },
-  { id: 5, userName: "noah", password: "noah" },
-  { id: 6, userName: "mehmet", password: "mehmet" },
-];
